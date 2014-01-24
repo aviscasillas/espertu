93b7754f6f2e8c6044fbc63ec1298849ce566eb2
Albert Viscasillas Campanyà <albert@wuaki.tv>
2014-01-24 18:29:40 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Espertu::Application.config.secret_key_base = 'cf662cb36b6205f7a56e5819c120a8ea9937d04ff11f8d61b192cfd93d42aade9292c8a59d74a0e450e70bbbb6cd695ff640ee4360e64dc41689ea0be62b234a'
